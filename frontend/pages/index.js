71c02357fb712b7ea70ac0449dfffae83a3e352f
mrunal-modi <mrunal.modi@gmail.com>
2022-07-18 19:56:00 +1000

    Final

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import CardB from "../components/CardB";
 import { RenderContent } from "../components/renderContent";
+import AppContext from "../components/context";
 
 export default function Home() {
+  const { user, setUser } = useContext(AppContext);
   return (
     <div className="home container">
       <div className="row">
@@ -11,12 +13,11 @@ export default function Home() {
             txtcolor="black"
             header={
               <div>
-                Welcome to the Bank
+                Welcome to the Bank {user?.displayName}
               </div>
             }
             // title="For all your banking needs"
             text="For all your banking needs"
-            body="Body"
           />
         </div>
 
