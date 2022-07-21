5e39731766f0f12ad79f554fb2d7b25d8e314ddb
mrunal-modi <mrunal.modi@gmail.com>
2022-07-21 19:49:14 +1000

    test

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,29 @@ const dal = require("./dal.js");
 
 // Firebase
 var admin = require("firebase-admin");
-// var serviceAccount = require("./secret/ServiceAccountKey.json");
+var serviceAccount = require("./secret/ServiceAccountKey.json");
+// Using admin secret from serviceAccount Json file
 // admin.initializeApp({
 //     credential: admin.credential.cert(serviceAccount),
 //     databaseURL:
 //         "https://fullstackbank-default-rtdb.firebaseio.com/",
 // });
 
+// Using admin secret from serviceAccount .env
 admin.initializeApp({
     credential: admin.credential.cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        type: process.env.NEXT_PUBLIC_TYPE,
+        project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+        private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+        private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+        auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
+        token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL,
       }),
-      databaseURL:
-        "https://fullstackbank-default-rtdb.firebaseio.com/",
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 });
 
 
