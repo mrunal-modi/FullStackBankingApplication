743308d70d57187dbce61e0d26533ab34aa87eea
mrunal-modi <mrunal.modi@gmail.com>
2022-07-21 21:57:01 +1000

    test

--- backend/index.js
+++ backend/index.js
@@ -19,16 +19,16 @@ var admin = require("firebase-admin");
 console.log(process.env);
 admin.initializeApp({
     credential: admin.credential.cert({
-        type: process.env.NEXT_PUBLIC_TYPE,
-        project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
-        private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
-        private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
-        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
-        auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
-        token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL,
+        type: process.env.NEXT_PUBLIC_FIREBASE_TYPE,
+        project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
+        auth_uri: process.env.NEXT_PUBLIC_FIREBASE_AUTH_URI,
+        token_uri: process.env.NEXT_PUBLIC_FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_X509_CERT_URL,
       }),
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 });
